57022ac3c7dba49cc726bc00206c958eb80e61d6
yzq <1149635502@qq.com>
2018-09-04 17:57:20 +0800

    新增FirstView 对基础使用进行注释

---
+++ app/src/main/java/com/yzq/customview/App.java
@@ -0,0 +1,22 @@
+package com.yzq.customview;
+
+import android.app.Application;
+
+import com.blankj.utilcode.util.LogUtils;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+        final LogUtils.Config config = LogUtils.getConfig()
+                .setLogSwitch(BuildConfig.DEBUG)// 设置 log 总开关，包括输出到控制台和文件，默认开
+                .setConsoleSwitch(BuildConfig.DEBUG)// 设置是否输出到控制台开关，默认开
+                .setSingleTagSwitch(true)// 一条日志仅输出一条，默认开，为美化 AS 3.1 的 Logcat
+                ;
+      
+
+    }
+}
---
+++ app/src/main/java/com/yzq/customview/ui/MainActivity.java
@@ -0,0 +1,20 @@
+package com.yzq.customview.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yzq.customview.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //FirstView firstView=new FirstView(this);
+
+    }
+
+
+}
